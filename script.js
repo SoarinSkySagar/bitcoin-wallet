a2c687189cee72b09d3ae9c9d5a9b69f53062843
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-05-29 00:49:20 +0530

    design build

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ var path = document.querySelector("#path");
 var prv = document.querySelector("#prv");
 var pub = document.querySelector("#pub");
 var qr = document.querySelector("#qr");
-var addressDisplay = document.getElementById("address");
+var addressDisplay = document.querySelector("#address");
+var btn = document.querySelector("#btn");
+var mp = document.querySelector("#m-p");
 
 var mnemonic = window.bsvMnemonic;
 var mnemonic1 = mnemonic.fromRandom();
-mnemonics.innerHTML = mnemonic1.toString();
+mp.innerHTML = mnemonic1.toString();
 
 var hdPrivateKey = bsv.HDPrivateKey.fromSeed(mnemonic1.toSeed());
 xprv.innerHTML = hdPrivateKey.toString();
@@ -18,20 +20,48 @@ xprv.innerHTML = hdPrivateKey.toString();
 var hdPublicKey = bsv.HDPublicKey.fromHDPrivateKey(hdPrivateKey);
 xpub.innerHTML = hdPublicKey.toString();
 
-if (path.value == false) {
-    var privateKey = hdPrivateKey.privateKey;
-    prv.innerHTML = privateKey;
+var privateKey = hdPrivateKey.privateKey;
+prv.innerHTML = privateKey;
 
-    var publicKey = bsv.PublicKey.fromPrivateKey(privateKey);
-    pub.innerHTML = publicKey;
-} else {
+var publicKey = bsv.PublicKey.fromPrivateKey(privateKey);
+pub.innerHTML = publicKey;
 
-}
+btn.addEventListener("click", ()=>{
+    var path_value = path.value;
+    if (path_value.length > 0) {
+        var privateKeyStandard1 = hdPrivateKey.deriveChild(path_value);
 
-let address = bsv.Address.fromPublicKey(publicKey).toString();
+        var privateKey = privateKeyStandard1.privateKey;
+        var publicKey = privateKeyStandard1.publicKey;
+
+        prv.innerHTML = privateKey;
+        pub.innerHTML = publicKey;
+
+        var address = bsv.Address.fromPublicKey(publicKey).toString();
+        addressDisplay.innerHTML = address;
+
+        var addressCode = 'bitcoinsv:' + address;
+        qr.innerHTML = '';
+        new QRCode(qr, addressCode);
+
+        var config = {
+            method: 'get',
+            url: "https://api.whatsonchain.com/v1/bsv/main/address/" +
+            address + "/balance",
+        };
+        axios(config).then((response) => {
+        let data = JSON.stringify(response.data);
+        balance.innerHTML = data;
+        });
+    } else {
+        alert("Please input a path!");
+    }
+});
+
+var address = bsv.Address.fromPublicKey(publicKey).toString();
 addressDisplay.innerHTML = address;
 
-let addressCode = 'bitcoinsv:' + address;
+var addressCode = 'bitcoinsv:' + address;
 new QRCode(qr, addressCode);
 
 var config = {
