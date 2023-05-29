825bd4ac79d107bfcf1c3934b4526e8262aaa744
/ <96525548+PedroHenriqueW@users.noreply.github.com>
2023-05-28 22:00:39 -0300

    Adding path validation by regex (Support only on bip-44 pattern)

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var qr = document.querySelector("#qr");
 var addressDisplay = document.querySelector("#address");
 var btn = document.querySelector("#btn");
 var mp = document.querySelector("#m-p");
+const errorText = document.getElementById("errorText");
 
 var mnemonic = window.bsvMnemonic;
 var mnemonic1 = mnemonic.fromRandom();
@@ -26,50 +27,70 @@ prv.innerHTML = privateKey;
 var publicKey = bsv.PublicKey.fromPrivateKey(privateKey);
 pub.innerHTML = publicKey;
 
-btn.addEventListener("click", ()=>{
-    var path_value = path.value;
-    if (path_value.length > 0) {
-        var privateKeyStandard1 = hdPrivateKey.deriveChild(path_value);
+// Support only on bip-44 pattern
+function testPath(value) {
+  const regexBip44 = /^m(\/\d+\'?)*$/;
 
-        var privateKey = privateKeyStandard1.privateKey;
-        var publicKey = privateKeyStandard1.publicKey;
+  if (regexBip44.test(value)) {
+    errorText.textContent = "";
+    return true;
+  } else {
+    errorText.textContent = "invalid format.";
+    return false;
+  }
+}
 
-        prv.innerHTML = privateKey;
-        pub.innerHTML = publicKey;
+path.addEventListener("input", () => {
+  const path_value = path.value;
+  testPath(path_value);
+});
 
-        var address = bsv.Address.fromPublicKey(publicKey).toString();
-        addressDisplay.innerHTML = address;
+btn.addEventListener("click", () => {
+  var path_value = path.value;
+  if (path_value.length > 0 && testPath(path_value)) {
+    var privateKeyStandard1 = hdPrivateKey.deriveChild(path_value);
 
-        var addressCode = 'bitcoinsv:' + address;
-        qr.innerHTML = '';
-        new QRCode(qr, addressCode);
+    var privateKey = privateKeyStandard1.privateKey;
+    var publicKey = privateKeyStandard1.publicKey;
 
-        var config = {
-            method: 'get',
-            url: "https://api.whatsonchain.com/v1/bsv/main/address/" +
-            address + "/balance",
-        };
-        axios(config).then((response) => {
-        let data = JSON.stringify(response.data);
-        balance.innerHTML = data;
-        });
-    } else {
-        alert("Please input a path!");
-    }
+    prv.innerHTML = privateKey;
+    pub.innerHTML = publicKey;
+
+    var address = bsv.Address.fromPublicKey(publicKey).toString();
+    addressDisplay.innerHTML = address;
+
+    var addressCode = "bitcoinsv:" + address;
+    qr.innerHTML = "";
+    new QRCode(qr, addressCode);
+
+    var config = {
+      method: "get",
+      url:
+        "https://api.whatsonchain.com/v1/bsv/main/address/" +
+        address +
+        "/balance",
+    };
+    axios(config).then((response) => {
+      let data = JSON.stringify(response.data);
+      balance.innerHTML = data;
+    });
+  } else {
+    alert("Please input a path!");
+  }
 });
 
 var address = bsv.Address.fromPublicKey(publicKey).toString();
 addressDisplay.innerHTML = address;
 
-var addressCode = 'bitcoinsv:' + address;
+var addressCode = "bitcoinsv:" + address;
 new QRCode(qr, addressCode);
 
 var config = {
-    method: 'get',
-    url: "https://api.whatsonchain.com/v1/bsv/main/address/" +
-    address + "/balance",
+  method: "get",
+  url:
+    "https://api.whatsonchain.com/v1/bsv/main/address/" + address + "/balance",
 };
 axios(config).then((response) => {
-   let data = JSON.stringify(response.data);
-   balance.innerHTML = data;
+  let data = JSON.stringify(response.data);
+  balance.innerHTML = data;
 });
